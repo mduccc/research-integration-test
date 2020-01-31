d2a034059468fbc1ba7b0c63e236f625984e37a2
mduccc <duc2521997@gmail.com>
2020-01-31 11:50:01 +0700

    Add empty test

---
+++ ios/flutter_integration_empty_test/flutter_integration_empty_test.swift
@@ -0,0 +1,33 @@
+//
+//  flutter_integration_empty_test.swift
+//  flutter_integration_empty_test
+//
+//  Created by DUCNM31 on 1/31/20.
+//  Copyright © 2020 The Chromium Authors. All rights reserved.
+//
+
+import XCTest
+
+class flutter_integration_empty_test: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
